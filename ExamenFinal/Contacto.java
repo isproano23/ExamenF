02d40b557146cddf13ec594cb80679abbfdf53bd
Aantony <alexanderpillalaza1997@gmail.com>
2026-03-26 20:14:04 -0500

    constructor

--- ExamenFinal/Contacto.java
+++ ExamenFinal/Contacto.java
@@ -6,5 +6,11 @@ public class Contacto {
     String descripcion;
     boolean exitosa;
 
+    public Transaccion(String tipo, double monto, String descripcion) {
+        this.tipo = tipo;
+        this.monto = monto;
+        this.descripcion = descripcion;
+        this.exitosa = true;
+    }
     
 }
