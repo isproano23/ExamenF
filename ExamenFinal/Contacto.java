e9a9b2a221f416e55553058099b549841a617918
Aantony <alexanderpillalaza1997@gmail.com>
2026-03-26 20:14:54 -0500

    subir mostrarDetalle

--- ExamenFinal/Contacto.java
+++ ExamenFinal/Contacto.java
@@ -12,5 +12,14 @@ public class Contacto {
         this.descripcion = descripcion;
         this.exitosa = true;
     }
+
+    public void mostrarDetalle() {
+        System.out.println("--- Detalle de Transacción ---");
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Monto: $" + monto);
+        System.out.println("Descripción: " + descripcion);
+        System.out.println("Estado: " + (exitosa ? "Exitosa" : "Fallida"));
+    }
+
     
 }
